c6e78c23c8f07dc9f6da9dbc80243eb06ca57356
PatcharidaP2350 <pimridaaa28@gmail.com>
2024-11-29 23:27:06 +0700

    Delete entity directory

--- entity/BloodGroup.go
+++
@@ -1,9 +0,0 @@
-package entity
-
-import "gorm.io/gorm"
-
-type BloodGroup struct {
-    gorm.Model
-    Name string `json:"name"` // เช่น A, B, AB, O
-	RHFactor string `json:"rh_factor"` // เช่น Rh+ หรือ Rh-
-}
